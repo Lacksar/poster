76027fabaa517f606b143006d07a580f5d3b0828
Lacksar <74723139+Lacksar@users.noreply.github.com>
2023-10-12 21:33:50 +0545

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -21,7 +21,7 @@ document.getElementById("joke").innerText= a;
 async function postJoke(){
 
    
-    let data = await fetch(`https://graph.facebook.com/2254937581441660/feed?message=${a}&access_token=EAAMPpQjRZArEBO0FlnLAHcPZCyuDVE5YQ5EZAxE6o6tlUqcbqxZANAZB7iFcaF922wRyhCTQoMUlaB9vbZChkHrdcUNTvpt3DNz19V6NUgZB3VdZAPkwdwtUQJfX1YVLaACiaPE9JZCT1inr0BkoxybOwCmiZBoiPezboaAZBQZC7uLnDymAly98EVvSIXQaYUCkJZBZCYTK8CjdOh5GCRlonHE73v5dqC4QZDZD`, {
+     let data = await fetch(`https://graph.facebook.com/2254937581441660/feed?message=${a}&access_token=EAAMPpQjRZArEBO0FlnLAHcPZCyuDVE5YQ5EZAxE6o6tlUqcbqxZANAZB7iFcaF922wRyhCTQoMUlaB9vbZChkHrdcUNTvpt3DNz19V6NUgZB3VdZAPkwdwtUQJfX1YVLaACiaPE9JZCT1inr0BkoxybOwCmiZBoiPezboaAZBQZC7uLnDymAly98EVvSIXQaYUCkJZBZCYTK8CjdOh5GCRlonHE73v5dqC4QZDZD`, {
      
     // Adding method type
     method: "POST"});
