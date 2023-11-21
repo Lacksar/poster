f1303105b8ce18601f05fceb1813fb78dc791913
Lacksar <74723139+Lacksar@users.noreply.github.com>
2023-11-21 16:54:58 +0545

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -21,7 +21,7 @@ document.getElementById("joke").innerText= a;
 async function postJoke(){
 
    
-     let data = await fetch(`https://graph.facebook.com/2254937581441660/feed?message=${a}&access_token=EAAMPpQjRZArEBO0FlnLAHcPZCyuDVE5YQ5EZAxE6o6tlUqcbqxZANAZB7iFcaF922wRyhCTQoMUlaB9vbZChkHrdcUNTvpt3DNz19V6NUgZB3VdZAPkwdwtUQJfX1YVLaACiaPE9JZCT1inr0BkoxybOwCmiZBoiPezboaAZBQZC7uLnDymAly98EVvSIXQaYUCkJZBZCYTK8CjdOh5GCRlonHE73v5dqC4QZDZD`, {
+     let data = await fetch(`https://graph.facebook.com/2254937581441660/feed?message=${a}&access_token=EAAMPpQjRZArEBO4ElF4CZASFI8u6j32matJtk4YhuUYKaDzJjeIy418PhA9dOzEq3TKgfnwe9FqXTAWMmzX0jPmcBPkjA5GuXJasmPF44ZCmK48qHLgnIfF8k23dEZCfPXZCRZC6kQZB4bjHc1qinKSHLK5J43ApuNhYcI4xS1ajF4uoG7wZBFFoOMbPyh8U1dwYLhAZD`, {
      
     // Adding method type
     method: "POST"});
